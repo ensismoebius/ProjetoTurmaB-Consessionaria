b8c2d4aeb0aa47e35b752d83697d8924f9381331
Luisa Sales <luisa.sales@etec.sp.gov.br>
2025-11-13 17:37:06 -0300

    Mudança da tabela PROPOSTAS
    foi necessário reparar alguns erros e adicionar a coluna TIPO. Também deletei colunas desnecessárias.

---
+++ src/PROPOSTAS.sql
@@ -0,0 +1,74 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost
+-- Tempo de geração: 13-Nov-2025 às 23:25
+-- Versão do servidor: 8.0.25
+-- versão do PHP: 7.3.28
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Banco de dados: `PRJ2DSB`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `PROPOSTAS`
+--
+
+CREATE TABLE `PROPOSTAS` (
+  `ID` int NOT NULL,
+  `VEICULO_ID` int DEFAULT NULL,
+  `NOME_CLIENTE` varchar(50) DEFAULT NULL,
+  `EMAIL_CLIENTE` varchar(50) DEFAULT NULL,
+  `TELEFONE_CLIENTE` char(15) DEFAULT NULL,
+  `DATA_PROPOSTA` timestamp NULL DEFAULT NULL,
+  `TIPO` enum('Agendar visita','Teste drive') NOT NULL DEFAULT 'Agendar visita'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Índices para tabelas despejadas
+--
+
+--
+-- Índices para tabela `PROPOSTAS`
+--
+ALTER TABLE `PROPOSTAS`
+  ADD PRIMARY KEY (`ID`),
+  ADD KEY `IDVEIC_VEICULO` (`VEICULO_ID`);
+
+--
+-- AUTO_INCREMENT de tabelas despejadas
+--
+
+--
+-- AUTO_INCREMENT de tabela `PROPOSTAS`
+--
+ALTER TABLE `PROPOSTAS`
+  MODIFY `ID` int NOT NULL AUTO_INCREMENT;
+
+--
+-- Restrições para despejos de tabelas
+--
+
+--
+-- Limitadores para a tabela `PROPOSTAS`
+--
+ALTER TABLE `PROPOSTAS`
+  ADD CONSTRAINT `IDVEIC_VEICULO` FOREIGN KEY (`VEICULO_ID`) REFERENCES `VEICULOS` (`id_veiculos`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
